a4cf198dc66e93cca7678d9a43c71d98812ea15f
Thanakrit Juthamongkhon <thanakrit.ju.work@gmail.com>
2020-11-01 21:11:28 +0700

    Add circles

--- src/features/canvas/Canvas.js
+++ src/features/canvas/Canvas.js
@@ -1,7 +1,10 @@
-import { useSelector } from 'react-redux';
+import React, { useRef, useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import {
   selectHeight,
   selectWidth,
+  selectCircles,
+  addCircle,
 } from './canvasSlice';
 import {
   selectColor,
@@ -10,8 +13,56 @@ import styles from './Canvas.module.css';
 
 export function Canvas() {
   const color = useSelector(selectColor);
+  const circles = useSelector(selectCircles);
+  const canvasRef = useRef(null);
+  const dispatch = useDispatch();
+
+  const draw = (ctx, frameCount) => {
+    ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+    circles.forEach((circle, index) => {
+      ctx.fillStyle = color;
+      ctx.beginPath();
+      ctx.arc(circle.x, circle.y, 20*Math.sin(frameCount*0.05)**2, 0, 2*Math.PI);
+      ctx.fill();
+      ctx.font = "20px Arial";
+      ctx.fillStyle = "#000";
+      ctx.fillText(index, circle.x, circle.y);
+    })
+  }
+
+  const onClick = (event) => {
+    const canvas = canvasRef.current;
+    let x = event.pageX - canvas.offsetLeft; 
+    let y = event.pageY - canvas.offsetTop; 
+    dispatch(addCircle({x, y}))
+  }
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const context = canvas.getContext('2d');
+    let frameCount = 0;
+    let animationFrameId;
+    
+    const render = () => {
+      frameCount++;
+      draw(context, frameCount);
+      animationFrameId = window.requestAnimationFrame(render);
+    }
+    render();
+    
+    return () => {
+      window.cancelAnimationFrame(animationFrameId);
+    }
+  }, [draw]);
 
   return (
-    <canvas style={{borderColor: color}} className={styles.canvas} width={useSelector(selectWidth)} height={useSelector(selectHeight)} />
+    <canvas 
+      style={{borderColor: color}} 
+      className={styles.canvas} 
+      ref={canvasRef} 
+      width={useSelector(selectWidth)} 
+      height={useSelector(selectHeight)} 
+      onMouseDown={(e) => onClick(e)}
+    />
   )
 }
--- src/features/canvas/canvasSlice.js
+++ src/features/canvas/canvasSlice.js
@@ -5,13 +5,24 @@ export const canvasSlice = createSlice({
   initialState: {
     width: "800",
     height: "600",
-    circles: []
+    circles: [
+      {x: 200, y: 300},
+      {x: 100, y: 500},
+      {x: 600, y: 200},
+      {x: 400, y: 400},
+    ]
   },
   reducers: {
+    addCircle: (state, action) => {
+      state.circles.push(action.payload);
+    },
   },
 });
 
+export const { addCircle } = canvasSlice.actions;
+
 export const selectWidth = state => state.canvas.width;
 export const selectHeight = state => state.canvas.height;
+export const selectCircles = state => state.canvas.circles;
 
 export default canvasSlice.reducer;
